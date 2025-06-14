9ad96963369f164fdffbd6390111920e6ef29d4d
CARLOTTO Riccardo (External) <riccardo.carlotto-ext@gucci.com>
2025-06-14 13:34:26 +0200

    Initial Commit

---
+++ Tamagoci wellness Watch App/Tamagoci_wellnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tamagoci_wellnessApp.swift
+//  Tamagoci wellness Watch App
+//
+//  Created by Riccardo Carlotto on 14/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Tamagoci_wellness_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
